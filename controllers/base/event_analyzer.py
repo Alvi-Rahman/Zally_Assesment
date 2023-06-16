e9e7f031c626b14f71c93b1e93416bf3a756248b
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2023-06-17 00:00:23 +0100

    add: documentation for the base classes

--- controllers/base/event_analyzer.py
+++ controllers/base/event_analyzer.py
@@ -1,12 +1,99 @@
+"""
+event_analyzer.py
+
+A Python module providing an EventAnalyzer base class.
+
+This module contains the `EventAnalyzer` class, which serves as a base class for implementing event analysis functionality.
+Specific event analyzers should inherit from this class and implement the abstract methods according to their specific requirements.
+
+Author: Takrim Rahman Albi
+
+"""
+
+
 class EventAnalyzer:
+    """
+    A base class for implementing event analysis functionality.
+
+    This class defines abstract methods that should be implemented by specific event analyzer subclasses.
+
+    Methods:
+        display_events(events):
+            Displays the events in a specific format.
+            Args:
+                events (list): The list of events to be displayed.
+
+        filter_events(events, event_type):
+            Filters the events based on the specified event type.
+            Args:
+                events (list): The list of events to be filtered.
+                event_type (str): The type of events to be included in the filter.
+
+        calculate_event_statistics(events):
+            Calculates statistics based on the provided events.
+            Args:
+                events (list): The list of events to be used for calculating statistics.
+
+        identify_most_active_user(events):
+            Identifies the most active user based on the provided events.
+            Args:
+                events (list): The list of events to be analyzed.
+
+    """
+
     def display_events(self, events):
+        """
+        Displays the events in a specific format.
+
+        This method should be implemented in the child class.
+
+        Args:
+            events (list): The list of events to be displayed.
+
+        Raises:
+            NotImplementedError: If the method is not implemented in the child class.
+        """
         raise NotImplementedError("display_events() method must be implemented in child class")
 
     def filter_events(self, events, event_type):
+        """
+        Filters the events based on the specified event type.
+
+        This method should be implemented in the child class.
+
+        Args:
+            events (list): The list of events to be filtered.
+            event_type (str): The type of events to be included in the filter.
+
+        Raises:
+            NotImplementedError: If the method is not implemented in the child class.
+        """
         raise NotImplementedError("filter_events() method must be implemented in child class")
 
     def calculate_event_statistics(self, events):
+        """
+        Calculates statistics based on the provided events.
+
+        This method should be implemented in the child class.
+
+        Args:
+            events (list): The list of events to be used for calculating statistics.
+
+        Raises:
+            NotImplementedError: If the method is not implemented in the child class.
+        """
         raise NotImplementedError("calculate_event_statistics() method must be implemented in child class")
 
     def identify_most_active_user(self, events):
+        """
+        Identifies the most active user based on the provided events.
+
+        This method should be implemented in the child class.
+
+        Args:
+            events (list): The list of events to be analyzed.
+
+        Raises:
+            NotImplementedError: If the method is not implemented in the child class.
+        """
         raise NotImplementedError("identify_most_active_user() method must be implemented in child class")
--- controllers/base/github_api.py
+++ controllers/base/github_api.py
@@ -1,11 +1,88 @@
+"""
+github_api.py
+
+A Python module providing a wrapper for the GitHub API.
+
+This module contains the `GitHubAPI` class, which allows fetching data from various endpoints of the GitHub API.
+
+Author: Takrim Rahman Albi
+
+Usage:
+    # Create an instance of GitHubAPI
+    api = GitHubAPI()
+
+    # Fetch data from a specific API endpoint
+    events = api.fetch("repos/owner/repo/events")
+
+    # Process the fetched data
+    for event in events:
+        # Perform actions on each event
+        print(event)
+"""
+
 import requests
 
 
 class GitHubAPI:
+    """
+    A Python wrapper for the GitHub API.
+
+    This class provides methods to interact with the GitHub API and fetch data from various endpoints.
+
+    Attributes:
+        base_url (str): The base URL of the GitHub API.
+
+    Methods:
+        __init__(base_url="https://api.github.com"):
+            Initializes a new instance of the GitHubAPI class.
+            Args:
+                base_url (str, optional): The base URL of the GitHub API. Defaults to "https://api.github.com".
+
+        fetch(endpoint):
+            Fetches data from the specified API endpoint.
+            Args:
+                endpoint (str): The API endpoint to fetch data from.
+            Returns:
+                dict: The JSON response received from the API endpoint.
+            Raises:
+                Exception: If the API request fails or returns a non-200 status code.
+
+    Example Usage:
+        # Create an instance of GitHubAPI
+        api = GitHubAPI()
+
+        # Fetch data from a specific API endpoint
+        events = api.fetch("repos/owner/repo/events")
+
+        # Process the fetched data
+        for event in events:
+            # Perform actions on each event
+            print(event)
+
+    """
+
     def __init__(self, base_url="https://api.github.com"):
+        """
+        Initializes a new instance of the GitHubAPI class.
+
+        Args:
+            base_url (str, optional): The base URL of the GitHub API. Defaults to "https://api.github.com".
+        """
         self.base_url = base_url
 
     def fetch(self, endpoint):
+        """
+        Fetches data from the specified API endpoint.
+
+        Args:
+            endpoint (str): The API endpoint to fetch data from.
+
+        Returns:
+            dict: The JSON response received from the API endpoint.
+
+        Raises:
+            Exception: If the API request fails or returns a non-200 status code.
+        """
         url = f"{self.base_url}/{endpoint}"
         response = requests.get(url)
 
