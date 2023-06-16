09554e0922d2c74667c9e6170234022ec3619419
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2023-06-17 00:06:37 +0100

    add: documentation for main.py

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 """
 GitHub Events API Analyzer
 
-This module contains the GitHubEventAnalyzer class that serves as the entry point for Even Analysis process.
+This module contains the `GitHubEventAnalyzer` class that serves as the entry point for the event analysis process.
 
 Classes:
-- GitHubEventAnalyzer
+- `GitHubEventAnalyzer`
 
 """
+
 import argparse
 
 from controllers.github_event_analyzer import GitHubEventsAnalyzerCLI
 from utils.custom_exception import InvalidInputException
 
 
-class GitHubEventAnalyzer:
+class GitHubEventAnalyzerEntryPoint:
     """
-    GitHubEventAnalyzer serves as the entry point for the Analysis process.
+    `GitHubEventAnalyzer` serves as the entry point for the event analysis process.
 
     Methods:
-        event_analyzer_entry_point():
-            Executes the privacy policy word counting process.
-
+        - `event_analyzer_entry_point(owner, repo, event_type, page, sort_order)`: Executes the event analysis process.
     """
 
     @staticmethod
-    def event_analyzer_entry_point(owner, repo, event_type, page, sort_order):
+    def entry_point(owner, repo, event_type, page, sort_order):
         """
-        Executes the process.
+        Executes the event analysis process.
+
+        Args:
+            owner (str): The owner of the GitHub repository.
+            repo (str): The name of the GitHub repository.
+            event_type (str): The type of events to filter (optional).
+            page (int): The page number for pagination.
+            sort_order (str): The sort order for events.
 
         Returns:
             None
 
+        Raises:
+            InvalidInputException: If both the repository owner and repository name are not provided.
+
+        Usage:
+        ```python
+        analyzer = GitHubEventAnalyzer()
+        analyzer.event_analyzer_entry_point("owner", "repo", event_type="PushEvent", page=1, sort_order="chronological")
+        ```
         """
         event_controller = GitHubEventsAnalyzerCLI()
         event_controller.fetch_and_display_events(owner, repo, event_type, page, sort_order)
@@ -53,8 +67,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     try:
-        analyzer = GitHubEventAnalyzer()
-        analyzer.event_analyzer_entry_point(args.owner, args.repo, args.event_type, args.page, args.sort_order)
+        analyzer = GitHubEventAnalyzerEntryPoint()
+        analyzer.entry_point(args.owner, args.repo, args.event_type, args.page, args.sort_order)
     except InvalidInputException as e:
         print(e)
     except Exception as e:
