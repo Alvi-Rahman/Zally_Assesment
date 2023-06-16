3e170b751b4614f2ca2051af44718510837dd83c
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2023-06-17 00:09:55 +0100

    add: documentations for the test file

--- tests/test_github_event_analyzer.py
+++ tests/test_github_event_analyzer.py
@@ -1,3 +1,13 @@
+"""
+GitHub Events API Analyzer Unit Tests
+
+This module contains unit tests for the GitHubEventsAnalyzer class.
+
+Classes:
+- TestEventAnalyzerController
+
+"""
+
 import pytest
 from unittest.mock import patch
 
@@ -5,12 +15,40 @@ from controllers.github_event_analyzer import GitHubEventsAnalyzer
 
 
 class TestEventAnalyzerController:
+    """
+    Unit tests for the EventAnalyzer controller class.
+
+    Methods:
+        event_controller(): Fixture to create an instance of GitHubEventsAnalyzer for testing.
+        test_fetch_events_success(): Test the fetch_events method for successful API response.
+        test_display_events(): Test the display_events method.
+        test_filter_events(): Test the filter_events method.
+        test_calculate_event_statistics(): Test the calculate_event_statistics method.
+        test_identify_most_active_user(): Test the identify_most_active_user method.
+
+    """
+
     @pytest.fixture
     def event_controller(self):
+        """
+        Fixture to create an instance of GitHubEventsAnalyzer for testing.
+
+        Returns:
+            An instance of GitHubEventsAnalyzer.
+
+        """
         return GitHubEventsAnalyzer()
 
     @patch('requests.get')
     def test_fetch_events_success(self, mock_get, event_controller):
+        """
+        Test the fetch_events method for successful API response.
+
+        Args:
+            mock_get: Mock object for the requests.get function.
+            event_controller: Instance of GitHubEventsAnalyzer.
+
+        """
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = [
             {'type': 'PushEvent', 'actor': {'login': 'user1'}, 'created_at': '2023-06-14T10:30:00Z'}]
@@ -23,6 +61,14 @@ class TestEventAnalyzerController:
         assert events[0]['created_at'] == '2023-06-14T10:30:00Z'
 
     def test_display_events(self, event_controller, capsys):
+        """
+        Test the display_events method.
+
+        Args:
+            event_controller: Instance of GitHubEventsAnalyzer.
+            capsys: Fixture to capture stdout.
+
+        """
         events = [{'type': 'PushEvent', 'actor': {'login': 'user1'}, 'created_at': '2023-06-14T10:30:00Z'},
                   {'type': 'PullRequestEvent', 'actor': {'login': 'user2'}, 'created_at': '2023-06-14T11:30:00Z'}]
 
@@ -40,6 +86,13 @@ class TestEventAnalyzerController:
         assert captured.out == expected_output
 
     def test_filter_events(self, event_controller):
+        """
+        Test the filter_events method.
+
+        Args:
+            event_controller: Instance of GitHubEventsAnalyzer.
+
+        """
         events = [{'type': 'PushEvent', 'actor': {'login': 'user1'}, 'created_at': '2023-06-14T10:30:00Z'},
                   {'type': 'PullRequestEvent', 'actor': {'login': 'user2'}, 'created_at': '2023-06-14T11:30:00Z'},
                   {'type': 'PushEvent', 'actor': {'login': 'user3'}, 'created_at': '2023-06-14T12:30:00Z'}]
@@ -53,6 +106,13 @@ class TestEventAnalyzerController:
         assert filtered_events[1]['actor']['login'] == 'user3'
 
     def test_calculate_event_statistics(self, event_controller):
+        """
+        Test the calculate_event_statistics method.
+
+        Args:
+            event_controller: Instance of GitHubEventsAnalyzer.
+
+        """
         events = [{'type': 'PushEvent'}, {'type': 'PullRequestEvent'}, {'type': 'PushEvent'}]
 
         event_counts = event_controller.calculate_event_statistics(events)
@@ -61,6 +121,13 @@ class TestEventAnalyzerController:
         assert event_counts.get('PullRequestEvent') == 1
 
     def test_identify_most_active_user(self, event_controller):
+        """
+        Test the identify_most_active_user method.
+
+        Args:
+            event_controller: Instance of GitHubEventsAnalyzer.
+
+        """
         events = [{'type': 'PushEvent', 'actor': {'login': 'user1'}},
                   {'type': 'PullRequestEvent', 'actor': {'login': 'user2'}},
                   {'type': 'PushEvent', 'actor': {'login': 'user1'}}]
