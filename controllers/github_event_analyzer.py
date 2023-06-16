caacf476a2c0194f47539e6dc85dd44c24661f8e
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2023-06-17 00:03:33 +0100

    add: documentation for github_event_analyzer

--- controllers/github_event_analyzer.py
+++ controllers/github_event_analyzer.py
@@ -1,15 +1,97 @@
+"""
+github_event_analyzer.py
+
+A Python module providing the GitHubEventsAnalyzer and GitHubEventsAnalyzerCLI classes.
+
+This module contains the `GitHubEventsAnalyzer` class, which is responsible for fetching GitHub events and performing event analysis.
+It also includes the `GitHubEventsAnalyzerCLI` class, which extends the `GitHubEventsAnalyzer` functionality to provide a command-line interface.
+
+Author: Takrim Rahman Albi
+
+Usage:
+    # Create an instance of GitHubEventsAnalyzer and call its methods
+    analyzer = GitHubEventsAnalyzer()
+    events = analyzer.fetch_events("owner", "repo")
+    analyzer.display_events(events)
+    filtered_events = analyzer.filter_events(events, "PushEvent")
+    event_statistics = analyzer.calculate_event_statistics(events)
+    most_active_user = analyzer.identify_most_active_user(events)
+
+    # Create an instance of GitHubEventsAnalyzerCLI and call its methods
+    cli = GitHubEventsAnalyzerCLI()
+    cli.fetch_and_display_events("owner", "repo", event_type="PushEvent", page=1, sort_order="chronological")
+"""
+
 from controllers.base.github_api import GitHubAPI
 from controllers.base.event_analyzer import EventAnalyzer
 from utils.custom_exception import InvalidInputException
 
 
 class GitHubEventsAnalyzer(GitHubAPI, EventAnalyzer):
+    """
+    A class for fetching GitHub events and performing event analysis.
+
+    This class inherits from GitHubAPI and EventAnalyzer, combining their functionality to analyze GitHub events.
+
+    Methods:
+        fetch_events(repo_owner, repo_name, page=1):
+            Fetches GitHub events for the specified repository.
+            Args:
+                repo_owner (str): The owner of the repository.
+                repo_name (str): The name of the repository.
+                page (int): The page number of the events to fetch.
+            Returns:
+                list: The list of fetched events.
+
+        display_events(events):
+            Displays the provided events.
+            Args:
+                events (list): The list of events to be displayed.
+
+        filter_events(events, event_type):
+            Filters the provided events based on the specified event type.
+            Args:
+                events (list): The list of events to be filtered.
+                event_type (str): The type of events to include in the filter.
+            Returns:
+                list: The filtered list of events.
+
+        calculate_event_statistics(events):
+            Calculates event statistics based on the provided events.
+            Args:
+                events (list): The list of events to be used for calculating statistics.
+            Returns:
+                dict: A dictionary containing event types as keys and their respective counts as values.
+
+        identify_most_active_user(events):
+            Identifies the most active user based on the provided events.
+            Args:
+                events (list): The list of events to be analyzed.
+            Returns:
+                str: The username of the most active user.
+
+    """
+
     def fetch_events(self, repo_owner, repo_name, page=1):
+        """
+        Fetches GitHub events for the specified repository.
+
+        Args:
+            repo_owner (str): The owner of the repository.
+            repo_name (str): The name of the repository.
+            page (int): The page number of the events to fetch.
+
+        Returns:
+            list: The list of fetched events.
+
+        Raises:
+            InvalidInputException: If both repo_owner and repo_name are not provided.
+        """
         endpoint = ""
         if repo_owner and not repo_name:
-            raise InvalidInputException("Both Repo Owner and Repo Name is required!")
+            raise InvalidInputException("Both Repo Owner and Repo Name are required!")
         elif not repo_owner and repo_name:
-            raise InvalidInputException("Both Repo Owner and Repo Name is required!")
+            raise InvalidInputException("Both Repo Owner and Repo Name are required!")
         elif repo_owner and repo_name:
             endpoint = f"repos/{repo_owner}/{repo_name}/"
 
@@ -17,6 +99,12 @@ class GitHubEventsAnalyzer(GitHubAPI, EventAnalyzer):
         return self.fetch(endpoint)
 
     def display_events(self, events):
+        """
+        Displays the provided events.
+
+        Args:
+            events (list): The list of events to be displayed.
+        """
         for event in events:
             print(f"Event: {event['type']}")
             print(f"User: {event['actor']['login']}")
@@ -24,10 +112,29 @@ class GitHubEventsAnalyzer(GitHubAPI, EventAnalyzer):
             print("-------------------------------------------")
 
     def filter_events(self, events, event_type):
+        """
+        Filters the provided events based on the specified event type.
+
+        Args:
+            events (list): The list of events to be filtered.
+            event_type (str): The type of events to include in the filter.
+
+        Returns:
+            list: The filtered list of events.
+        """
         filtered_events = [event for event in events if event['type'] == event_type]
         return filtered_events
 
     def calculate_event_statistics(self, events):
+        """
+        Calculates event statistics based on the provided events.
+
+        Args:
+            events (list): The list of events to be used for calculating statistics.
+
+        Returns:
+            dict: A dictionary containing event types as keys and their respective counts as values.
+        """
         event_counts = {}
         for event in events:
             event_type = event['type']
@@ -35,6 +142,15 @@ class GitHubEventsAnalyzer(GitHubAPI, EventAnalyzer):
         return event_counts
 
     def identify_most_active_user(self, events):
+        """
+        Identifies the most active user based on the provided events.
+
+        Args:
+            events (list): The list of events to be analyzed.
+
+        Returns:
+            str: The username of the most active user.
+        """
         user_counts = {}
         for event in events:
             username = event['actor']['login']
@@ -45,7 +161,35 @@ class GitHubEventsAnalyzer(GitHubAPI, EventAnalyzer):
 
 
 class GitHubEventsAnalyzerCLI(GitHubEventsAnalyzer):
+    """
+    A class that extends GitHubEventsAnalyzer to provide a command-line interface for analyzing GitHub events.
+
+    This class inherits from GitHubEventsAnalyzer and provides additional methods for fetching and displaying events
+    along with event statistics.
+
+    Methods:
+        fetch_and_display_events(repo_owner, repo_name, event_type=None, page=1, sort_order='chronological'):
+            Fetches GitHub events, filters them based on event type, and displays them along with event statistics.
+            Args:
+                repo_owner (str): The owner of the repository.
+                repo_name (str): The name of the repository.
+                event_type (str): The type of events to filter (optional).
+                page (int): The page number of the events to fetch.
+                sort_order (str): The sort order of the events ('chronological' or 'reverse-chronological').
+
+    """
+
     def fetch_and_display_events(self, repo_owner, repo_name, event_type=None, page=1, sort_order='chronological'):
+        """
+        Fetches GitHub events, filters them based on event type, and displays them along with event statistics.
+
+        Args:
+            repo_owner (str): The owner of the repository.
+            repo_name (str): The name of the repository.
+            event_type (str): The type of events to filter (optional).
+            page (int): The page number of the events to fetch.
+            sort_order (str): The sort order of the events ('chronological' or 'reverse-chronological').
+        """
         events = self.fetch_events(repo_owner, repo_name, page)
         if event_type:
             events = self.filter_events(events, event_type)
